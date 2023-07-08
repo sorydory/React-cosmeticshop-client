db81f700658beaf52488308f0fd334fdcbb5d8e1
sorydory <ejdbelle12@naver.com>
2023-07-09 02:23:06 +0900

    update: modify sql

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,28 @@
-import axios from 'axios';
-import React from 'react';
-import useAsync from '../customHook/useAsync';
+import axios from "axios";
+import React from "react";
+import useAsync from "../customHook/useAsync";
 
-async function getProducts(){
-    const response = await axios.get(
-        'http://localhost:8080/products'
-    );
-    return response.data;
+async function getProducts() {
+  const response = await axios.get("http://localhost:8081/products");
+  return response.data;
 }
 const Products = () => {
-    const state = useAsync(getProducts,[]);
-    const { loading, data, error } = state;
-    if (loading) return <div>로딩중....</div>;
-    if (error) return <div>에러가 발생했습니다.</div>;
-    if (!data) return null;
-    return (
+  const state = useAsync(getProducts, []);
+  const { loading, data, error } = state;
+  if (loading) return <div>로딩중....</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!data) return null;
+  return (
+    <div>
+      {data.products.map((pr) => (
         <div>
-            {data.products.map(pr => <div><p>{pr.name}</p>
-            <p>{pr.price}</p><p>{pr.seller}</p></div>)}
+          <p>{pr.name}</p>
+          <p>{pr.price}</p>
+          <p>{pr.seller}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Products;
--- src/main/index.js
+++ src/main/index.js
@@ -1,39 +1,41 @@
-import axios from 'axios';
-import React from 'react';
-import ProductList from '../components/ProductList';
-import useAsync from '../customHook/useAsync';
-import './index.css';
+import axios from "axios";
+import React from "react";
+import ProductList from "../components/ProductList";
+import useAsync from "../customHook/useAsync";
+import "./index.css";
 
-async function productFetch(){
-    const response = await axios.get('http://localhost:8080/products');
-    return response.data;
+async function productFetch() {
+  const response = await axios.get("http://localhost:8081/products");
+  return response.data;
 }
 const MainPage = () => {
-    const state = useAsync(productFetch,[]);
-    const { loading, data, error} = state;
-    if(loading) return <div>로딩중...</div>;
-    if(error) return <div>에러가 발생했습니다.</div>;
-    if(!data) return null;
-    console.log(data);
-    return (
-        <div className='main'>
-            <div className='visual'>
-                <img src="images/main_banner1.png" alt="배너이미지" />
-            </div>
-            <div className='product'>
-                <h2>신상품</h2>
-                <ul>
-                    {data.map(pro=> 
-                    <ProductList key={pro.p_id} 
-                    p_id={pro.p_id} 
-                    p_name={pro.p_name} 
-                    p_price={pro.p_price}
-                    p_img = {pro.p_img}
-                    />)}
-                </ul>
-            </div>
-        </div>
-    );
+  const state = useAsync(productFetch, []);
+  const { loading, data, error } = state;
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!data) return null;
+  console.log(data);
+  return (
+    <div className="main">
+      <div className="visual">
+        <img src="images/main_banner1.png" alt="배너이미지" />
+      </div>
+      <div className="product">
+        <h2>신상품</h2>
+        <ul>
+          {data.map((pro) => (
+            <ProductList
+              key={pro.p_id}
+              p_id={pro.p_id}
+              p_name={pro.p_name}
+              p_price={pro.p_price}
+              p_img={pro.p_img}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
